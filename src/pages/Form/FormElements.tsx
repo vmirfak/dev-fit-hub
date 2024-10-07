e7288a42e87010f25eb261706c79faad264fc269
Pedro Moreira(uie54339) <pedro.moreira@conti-engineering.com>
2024-10-07 19:42:06 +0100

    Fix defaultlayout

--- src/pages/Form/FormElements.tsx
+++ src/pages/Form/FormElements.tsx
@@ -8,7 +8,7 @@ import SwitcherFour from '../../components/Switchers/SwitcherFour';
 import SwitcherOne from '../../components/Switchers/SwitcherOne';
 import SwitcherThree from '../../components/Switchers/SwitcherThree';
 import SwitcherTwo from '../../components/Switchers/SwitcherTwo';
-import DefaultLayout from '../../layout/DefaultLayout';
+import DefaultLayout from '../../layout/DefaultLoayout';
 import DatePickerOne from '../../components/Forms/DatePicker/DatePickerOne';
 import DatePickerTwo from '../../components/Forms/DatePicker/DatePickerTwo';
 import SelectGroupTwo from '../../components/Forms/SelectGroup/SelectGroupTwo';
--- src/pages/Form/FormLayout.tsx
+++ src/pages/Form/FormLayout.tsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom';
 import Breadcrumb from '../../components/Breadcrumbs/Breadcrumb';
 import SelectGroupOne from '../../components/Forms/SelectGroup/SelectGroupOne';
-import DefaultLayout from '../../layout/DefaultLayout';
+import DefaultLayout from '../../layout/DefaultLoayout';
 
 const FormLayout = () => {
   return (
