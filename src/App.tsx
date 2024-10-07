dc9d382ddbedbb2952f4473fa2d6ef8d8e0713cf
Pedro Moreira(uie54339) <pedro.moreira@conti-engineering.com>
2024-10-07 18:12:07 +0100

    Setup the login page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Registration from "./pages/Authentication/Registration";
+import Login from "./pages/Authentication/Login";
+import { Route, Routes, useLocation } from 'react-router-dom';
+import Loader from "./common/Loader";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [loading, setLoading] = useState<boolean>(false);
+  const { pathname } = useLocation();
 
-  return (
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return loading ? (
+    <Loader />
+  ) : (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Routes>
+        <Route path="/" element={<Login setLoading={setLoading} />} />
+        <Route path="/register" element={<Registration />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
