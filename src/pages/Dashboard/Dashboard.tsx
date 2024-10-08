27d9616c55881f656a6c75b726f468af9077a822
Pedro Moreira(uie54339) <pedro.moreira@conti-engineering.com>
2024-10-08 17:55:16 +0100

    make vercel.json file.

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -2,17 +2,17 @@ import React from 'react';
 import DefaultLayout from '../../layout/DefaultLoayout';
 import Breadcrumb from '../../components/Breadcrumbs/Breadcrumb';
 
+
 const MyDashboard: React.FC = () => {
   return (
     <DefaultLayout>
       <Breadcrumb pageName="My Home Page" />
-      <div className="overflow-hidden rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
-        <div >
+      <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
+        
 
-        </div>
       </div>
     </DefaultLayout>
   );
-    };
+};
 
 export default MyDashboard;
