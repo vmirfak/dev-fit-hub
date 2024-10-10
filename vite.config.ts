4f10227ec2cf3c99b84a890e4257532f8c9a5ff4
Pedro Moreira(uie54339) <pedro.moreira@conti-engineering.com>
2024-10-10 10:20:59 +0100

    Correct vite confg.

--- vite.config.ts
+++ vite.config.ts
@@ -32,12 +32,6 @@ export default defineConfig({
             purpose: 'apple touch icon',
           },
           {
-            src: '/icon-144x144.png',
-            sizes: '144x144',
-            type: 'image/png',
-            purpose: 'any',
-          },
-          {
             src: '/icon-256x256.png',
             sizes: '256x256',
             type: 'image/png',
